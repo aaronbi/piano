3184834d98dd6100c493f75b4f40685800afa401
aaronbi <99830616+aaronbi@users.noreply.github.com>
2022-02-16 12:35:24 -0600

    adding process_event to Controller

--- simple_controller.py
+++ simple_controller.py
@@ -13,9 +13,4 @@ class SimpleController(Controller):
         message, deltatime = event
         state = message[0]
         print(message, deltatime)
-        if state == SimpleController.DOWN:
-            self.pixels[self.next_light % self.num_lights] = self.color_on
-            self.next_light+=1
-        else:
-            self.pixels[self.prev_light % self.num_lights] = self.color_off
-            self.prev_light+=1
+        Controller.process_event(self, event)
